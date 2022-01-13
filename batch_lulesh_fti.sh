5d46b6f4118106ff7e16ee4526597ae28b293e78
tj-laser <johnson2319@login1.ufhpc>
2022-01-13 13:24:25 -0500

    Edits to the batch_lulesh_fti.sh file: Remove the partition line (as it seems the partition names changed, and there's a default anyway), removed the name of the log file (so each would be unique), added the module load (ml) line, and recompiled. Runs fine now.

--- batch_lulesh_fti.sh
+++ batch_lulesh_fti.sh
@@ -1,13 +1,15 @@
 #!/bin/bash
 #SBATCH -N 8
-#SBATCH -p pbatch
+##SBATCH -p pbatch
 #SBATCH -t 00:01:00
 ##SBATCH --mem-per-cpu=1G
 ##SBATCH --ntasks=1
 ##SBATCH --ntasks-per-node=1
 #SBATCH -V
 #SBATCH -J lulesh2_test
-#SBATCH -o log_fti.txt
+##SBATCH -o log_fti.txt
+
+ml intel/2020 openmpi/4.0.3 silo visit hdf5 boost/1.71
 
 #srun -n 1 lulesh2.0 -s 20 -i 1000
 srun -n 8 lulesh2.0 -s 10
